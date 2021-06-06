1b907dbc315995cd6cd47e939f026d3e06be0cd9
Lionek95 <kate_1995@tlen.pl>
2021-06-07 01:28:25 +0200

    bug fixes

--- src/index.js
+++ src/index.js
@@ -1,26 +1,51 @@
 import './style.css';
 import $ from 'jquery';
 
-$('.required').on('input', function () {
-    var empty = false;
-    $('.required').each(function() {
-      if ($(this).val() == '') {
-        empty = true;
-      }
-    });
+//Choice of the amount
 
-    if (empty) {
-      $('#send').attr('disabled', 'disabled');
-    } 
-    else {
+$(function(){
+  $('.amount__value').on('click', function(){
+    $(this).toggleClass('active');
+    $('.amount__value').not(this).removeClass('active');
+  });
+});
+
+//Form validation
+
+$(function(){
+  $('.shipping input').on('blur', function(){
+      if(!$(this).val()){
+          $(this).addClass('error');
+      } else{
+          $(this).removeClass('error');
+      }
+
+      if(!$(this).val()){
+        $('.fill__fields--info').css('display', 'block');
+      } else{
+        $('.fill__fields--info').css('display', 'none');
+      }
+  });
+});
+
+
+
+
+$(function(){
+  $('input[type="checkbox"]').on('click', function(){
+    if($(this).prop('checked') == true) {
       $('#send').removeAttr('disabled');
     }
-
-    if (empty) {
-      $('.required').css('border-color', 'red');
+    else if($(this).prop('checked') == false) {
+      $('#send').attr('disabled', 'disabled');
     }
-    else {
-      $('.required').css('border-color', 'gray');
-    }
+  });
+});
 
+//The code after submition
+
+$(function(){
+  $('#send').on('click', function(){
+    $('.code__text').css('display', 'block');
+  });
 });
